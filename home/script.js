ea8396d4afa7497b480dda5c95f6a7eff350d158
veiga <71990656+gustavovianaveiga@users.noreply.github.com>
2022-12-21 16:29:58 -0300

    layout

--- home/script.js
+++ home/script.js
@@ -18,3 +18,24 @@ function opennav() {
     const nav = document.querySelector('nav');
     nav.style.left = '0px'
 }
+//grafico pizza
+const ctx = document.getElementById('myChart');
+
+new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
