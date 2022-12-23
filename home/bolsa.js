b88a557aea7c9e0b7122d2c9615ad673d35f8231
veiga <71990656+gustavovianaveiga@users.noreply.github.com>
2022-12-23 17:46:29 -0300

    b2b

--- home/bolsa.js
+++ home/bolsa.js
@@ -1,40 +1,48 @@
-//function buscarcodigo() {
-fetch('https://fintz.herokuapp.com/api/b3/proventos?ticker=PETR4&size=15&sort=dataPagamento,desc').then(resposta => {
-    return resposta.json()
-}).then(function(corpo) {
-
-    let value = 0
-
-    for (let i = 0; i < corpo.length; i++) {
-        value = parseFloat(corpo[i].valor) + value
-    }
-    console.log("a PETR4 pagou R$" + value.toFixed(2) + " no ultimo ano");
-    console.log("cerca de R$" + (value / 12).toFixed(2) + " por mês");
+function buscar() {
+    const nome = document.querySelector('#nome');
+    const ticket = document.querySelector('#ticket');
+    const valor = document.querySelector('#valor');
+    const ticketbusca = document.querySelector('#ticketbusca').value;
+    const ultimodividendo = document.querySelector('#ultimodividendo');
+    const data = document.querySelector('#data');
 
 
-})
-fetch('https://fintz.herokuapp.com/api/b3/proventos?size=2000&sort=valor,desc&tipo=Dividendo').then(resposta => {
-    return resposta.json()
-}).then(function(corpo2) {
+    fetch('https://fintz.herokuapp.com/api/b3/proventos?ticker=' +
+        ticketbusca + '&size=15&sort=dataPagamento,desc').then(resposta => {
+        return resposta.json()
+    }).then(function(corpo) {
+
+        let value = 0
+
+        for (let i = 0; i < corpo.length; i++) {
+            value = parseFloat(corpo[i].valor) + value
+        }
+        console.log("a " + ticketbusca + " pagou R$" + value.toFixed(2) + " no ultimo ano");
+        console.log("cerca de R$" + (value / 12).toFixed(2) + " por mês");
+
+        // altaração da tabela info
 
 
-    console.log(corpo2);
-
-})
-fetch('https://raw.githubusercontent.com/thefintz/docs/main/Endpoints-Fintz.postman_collection.json').then(resposta => {
-    return resposta.json()
-}).then(function(corpo2) {
 
 
-    console.log(corpo2.item[0].item[1].request.url);
+    })
+    fetch('https://fintz.herokuapp.com/api/b3/proventos?ticker=' + ticketbusca + '&size=1&sort=dataCom,desc').then(resposta => {
+        return resposta.json()
+    }).then(function(corpo) {
 
-})
-fetch('https://fintz.herokuapp.com/api/b3/ativos/cotacoes').then(resposta => {
+        nome.innerHTML = (corpo[0].nomeEmpresa).toLowerCase();
+        ultimodividendo.innerHTML = "R$" + parseFloat(corpo[0].valor).toFixed(2)
+        data.innerHTML = corpo[0].dataPagamento
+        console.log(corpo);
+    })
+
+    fetch('https://fintz.herokuapp.com/api/b3/acoes/' + ticketbusca + "/preco").then(resposta => {
         return resposta.json()
     }).then(function(corpo2) {
 
+        ticket.innerHTML = corpo2.ticker
+        valor.innerHTML = "R$" + corpo2.preco
 
-        console.log(corpo2);
 
     })
-    //}
+}
--- home/script.js
+++ home/script.js
@@ -1,12 +1,3 @@
-//hamburger animation
-const hamburger = document.querySelector('.hamburger');
-hamburger.addEventListener('click', () => {
-    if (hamburger.classList == 'hamburger hamburger--spin') {
-        hamburger.classList = 'hamburger hamburger--spin is-active'
-    } else {
-        hamburger.classList = 'hamburger hamburger--spin'
-    }
-});
 //navbar
 
 function closenav() {
