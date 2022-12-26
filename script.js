fcc17d084001183765e096013154ab4177569723
veiga <71990656+gustavovianaveiga@users.noreply.github.com>
2022-12-26 17:51:11 -0300

    b2b 26/12

---
+++ script.js
@@ -0,0 +1,11 @@
+//navbar
+
+function closenav() {
+    const nav = document.querySelector('nav');
+    nav.style.left = '-300px'
+}
+
+function opennav() {
+    const nav = document.querySelector('nav');
+    nav.style.left = '0px'
+}
