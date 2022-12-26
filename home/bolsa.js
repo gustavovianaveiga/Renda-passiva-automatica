43cf7314acdd2107d150b34147f1ba13321a026e
veiga <71990656+gustavovianaveiga@users.noreply.github.com>
2022-12-26 17:52:36 -0300

    b2b 26;12

--- home/bolsa.js
+++ home/bolsa.js
@@ -5,6 +5,9 @@ function buscar() {
     const ticketbusca = document.querySelector('#ticketbusca').value;
     const ultimodividendo = document.querySelector('#ultimodividendo');
     const data = document.querySelector('#data');
+    const DY = document.querySelector('#DY');
+    const numerodeativonecessario = document.querySelector('#numerodeativonecessario');
+    const rendadesejada = document.querySelector('#rendadesejada').value;
 
 
     fetch('https://fintz.herokuapp.com/api/b3/proventos?ticker=' +
@@ -26,22 +29,47 @@ function buscar() {
 
 
     })
-    fetch('https://fintz.herokuapp.com/api/b3/proventos?ticker=' + ticketbusca + '&size=1&sort=dataCom,desc').then(resposta => {
+    fetch('https://fintz.herokuapp.com/api/b3/proventos?ticker=' + ticketbusca + '&size=12&sort=dataCom,desc&tipo=Dividendo').then(resposta => {
         return resposta.json()
     }).then(function(corpo) {
 
         nome.innerHTML = (corpo[0].nomeEmpresa).toLowerCase();
-        ultimodividendo.innerHTML = "R$" + parseFloat(corpo[0].valor).toFixed(2)
+        let value = 0
+
+        for (let i = 0; i < corpo.length; i++) {
+            value = parseFloat(corpo[i].valor) + value
+        }
+        media = value / corpo.length
+        ultimodividendo.innerHTML = "R$" + (media).toFixed(2)
         data.innerHTML = corpo[0].dataPagamento
+
+        fetch('https://fintz.herokuapp.com/api/b3/acoes/' + ticketbusca + "/preco").then(resposta => {
+            return resposta.json()
+        }).then(function(corpo2) {
+
+            ticket.innerHTML = corpo2.ticker
+            valor.innerHTML = "R$" + corpo2.preco
+
+
+            const ativosnecessariosresult = (rendadesejada / media).toFixed(0)
+            numerodeativonecessario.innerHTML = "Nº de " + corpo[0].categoria + "necessárias: " + ativosnecessariosresult
+        })
+
+
+
+
         console.log(corpo);
+        console.log(media);
     })
 
-    fetch('https://fintz.herokuapp.com/api/b3/acoes/' + ticketbusca + "/preco").then(resposta => {
+
+    fetch('https://fintz.herokuapp.com/api/b3/acoes/' + ticketbusca + "/indicadores").then(resposta => {
         return resposta.json()
     }).then(function(corpo2) {
 
-        ticket.innerHTML = corpo2.ticker
-        valor.innerHTML = "R$" + corpo2.preco
+        DY.innerHTML = parseFloat(corpo2.dy).toFixed(2) + "%"
+
+        console.log(corpo2);
 
 
     })
